59f126071bc1b24bf1f318f93a230ff42ed765a3
Wladimir Palant <fqcgithub@palant.de>
2023-04-19 17:36:32 +0200

    Added validation of KDF parameters

--- keepass_db/src/random.rs
+++ keepass_db/src/random.rs
@@ -1,8 +1,7 @@
 use crate::error::Error;
 
 pub fn random_vec(size: usize) -> Result<Vec<u8>, Error> {
-    let mut vec = Vec::with_capacity(size);
-    vec.resize(size, 0);
+    let mut vec = vec![0; size];
     getrandom::getrandom(&mut vec).or(Err(Error::RandomNumberGeneratorFailed))?;
     Ok(vec)
 }
--- native_host/src/main.rs
+++ native_host/src/main.rs
@@ -58,11 +58,11 @@ fn choose_path(save: bool) -> Result<PathBuf, Error> {
 }
 
 fn create_database(path: &PathBuf) -> Result<(), Error> {
-    let mut memory = 1024 * 1024;
+    let mut memory = 1024;
     let mut parallelism = num_cpus::get() as u32;
     let mut seconds = 1.0;
     let selection = Select::new()
-        .with_prompt(format!("Use default key derivation parameters ({memory} KB, {parallelism} threads, {seconds} seconds to unlock)"))
+        .with_prompt(format!("Use default key derivation parameters ({memory} MiB, {parallelism} threads, {seconds} seconds to unlock)"))
         .items(&["Yes", "No"])
         .default(0)
         .interact_opt()?
@@ -70,22 +70,31 @@ fn create_database(path: &PathBuf) -> Result<(), Error> {
     if selection != 0 {
         memory = Input::new()
             .with_prompt(
-                "Enter the amount of memory in kB used for key derivation (more is better)",
+                "Enter the amount of memory in MiB to be used for key derivation (more is better)",
             )
             .default(memory)
-            .interact_text()?;
+            .interact_text()?
+            .max(1);
         parallelism = Input::new()
             .with_prompt(
                 "Enter the number of parallel threads (ideally the number of your CPU cores)",
             )
             .default(parallelism)
-            .interact_text()?;
+            .interact_text()?
+            .max(1);
         seconds = Input::new()
             .with_prompt("Enter the time in seconds spent unlocking the database (more is better)")
             .default(seconds)
+            .validate_with(|input: &f32| {
+                if *input >= 0.1 && *input <= 10.0 {
+                    Ok(())
+                } else {
+                    Err("Please enter a number between 0.1 and 10")
+                }
+            })
             .interact_text()?;
     }
-    let kdf_parameters = keepass_db::KdfParameters::generate(memory, parallelism, seconds)?;
+    let kdf_parameters = keepass_db::KdfParameters::generate(memory * 1024, parallelism, seconds)?;
 
     let main_password = Password::new()
         .with_prompt("Main password for the database")
@@ -120,7 +129,7 @@ fn setup_database() -> Result<(), Error> {
         Some(path) => {
             eprintln!("Currently configured database file is {}.", path.display());
             let selection = Select::new()
-                .with_prompt("Keep this database file")
+                .with_prompt("Keep using this database file")
                 .items(&["Yes", "No"])
                 .default(0)
                 .interact_opt()?
