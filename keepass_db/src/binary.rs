2f6767599a0365bc81559f58ac3bd68a0caf9a82
Wladimir Palant <fqcgithub@palant.de>
2023-05-03 16:05:36 +0200

    Added tests and documentation to Binary type

--- keepass_db/src/binary.rs
+++ keepass_db/src/binary.rs
@@ -3,13 +3,18 @@ use std::io::{Read, Write};
 use crate::io::{Deserialize, DeserializeWithSize, Serialize};
 use crate::Error;
 
-#[derive(Debug)]
+/// Holds the data of a binary attachment
+#[derive(Debug, PartialEq)]
 pub(crate) struct Binary {
+    /// Attachment flags, 0x01 indicates that memory protection should be enabled (ignored).
     flags: u8,
+
+    /// Binary file data
     data: Vec<u8>,
 }
 
 impl Binary {
+    /// Calculates the size of this data structure when serialized in a KeePass database.
     pub fn len(&self) -> usize {
         (u8::BITS / 8) as usize + self.data.len()
     }
@@ -35,5 +40,91 @@ impl DeserializeWithSize for Binary {
 
         let data = Vec::deserialize(input, size - 1)?;
         Ok(Self { flags, data })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use hex_literal::hex;
+    use std::io::Cursor;
+
+    fn deserialize<const SIZE: usize>(data: [u8; SIZE]) -> Result<Binary, Error> {
+        let mut cursor = Cursor::new(data);
+        Binary::deserialize(&mut cursor, SIZE)
+    }
+
+    fn serialize(binary: Binary) -> Result<Vec<u8>, Error> {
+        let mut result = Vec::new();
+        binary.serialize(&mut result)?;
+        Ok(result)
+    }
+
+    #[test]
+    fn test_deserialize() {
+        assert!(matches!(
+            deserialize(hex!("")).expect_err("Deserializing empty data"),
+            Error::InvalidFieldSize
+        ));
+
+        assert!(matches!(
+            deserialize(hex!("03")).expect_err("Deserializing invalid flag"),
+            Error::UnsupportedBinaryFlags(0x03)
+        ));
+
+        assert_eq!(
+            deserialize(hex!("00")).expect("Deserializing flag only"),
+            Binary {
+                flags: 0x00,
+                data: vec![],
+            }
+        );
+
+        assert_eq!(
+            deserialize(hex!("01 02 03 04")).expect("Deserializing flag with data"),
+            Binary {
+                flags: 0x01,
+                data: hex!("02 03 04").to_vec(),
+            }
+        );
+    }
+
+    #[test]
+    fn test_serialize() {
+        assert_eq!(
+            serialize(Binary {
+                flags: 0x00,
+                data: vec![],
+            })
+            .expect("Serializing flag only"),
+            hex!("00"),
+        );
+
+        assert_eq!(
+            Binary {
+                flags: 0x00,
+                data: vec![],
+            }
+            .len(),
+            hex!("00").len(),
+        );
+
+        assert_eq!(
+            serialize(Binary {
+                flags: 0x01,
+                data: hex!("02 03 04").to_vec(),
+            })
+            .expect("Serializing flag with data"),
+            hex!("01 02 03 04"),
+        );
+
+        assert_eq!(
+            Binary {
+                flags: 0x01,
+                data: hex!("02 03 04").to_vec(),
+            }
+            .len(),
+            hex!("01 02 03 04").len(),
+        );
     }
 }
