e6dbb730378c1cf15c6d25700714885a593069c7
Wladimir Palant <fqcgithub@palant.de>
2023-05-08 11:17:43 +0200

    Added documentation and tests for BlockCipher and StreamCipher types

--- keepass_db/src/cipher.rs
+++ keepass_db/src/cipher.rs
@@ -11,17 +11,23 @@ const UUID_CHACHA20: [u8; 16] = hex_literal::hex!("d6038a2b 8b6f 4cb5 a524 339a3
 const AES_BLOCK_SIZE: usize = 16;
 const TWOFISH_BLOCK_SIZE: usize = 16;
 
+/// The KeePass database encryption approach as specified in the outer header.
 #[derive(Debug, Default, PartialEq)]
 pub(crate) enum BlockCipher {
+    /// AES256-CBC with PKCS7 padding
     Aes256,
+    /// Twofish-CBC with PKCS7 padding
     Twofish,
     #[default]
+    /// ChaCha20 algorithm
     ChaCha20,
 }
 
 impl BlockCipher {
+    /// Size of the outer header field value
     pub const SIZE: usize = UUID_AES256.len();
 
+    /// Returns the size of the initialization vector for the selected algorithm
     pub fn iv_size(&self) -> usize {
         match self {
             Self::Aes256 => 16,
@@ -30,6 +36,8 @@ impl BlockCipher {
         }
     }
 
+    /// Decrypts a block of data in place with the selected algorithm, key and
+    /// initialization vector. Might result in Error::DecryptionError.
     pub fn decrypt(&self, data: &mut Vec<u8>, key: &[u8], iv: &[u8]) -> Result<(), Error> {
         match self {
             Self::Aes256 => {
@@ -59,6 +67,8 @@ impl BlockCipher {
         Ok(())
     }
 
+    /// Encrypts a block of data in place with the selected algorithm, key and
+    /// initialization vector. Might result in Error::EncryptionError.
     pub fn encrypt(&self, data: &mut Vec<u8>, key: &[u8], iv: &[u8]) -> Result<(), Error> {
         match self {
             Self::Aes256 => {
@@ -119,15 +129,22 @@ impl Deserialize for BlockCipher {
 
 numeric_enum! {
     #[derive(Default)]
+    /// The stream cipher used for protected fields as specified in the inner header
+    /// of the KeePass database.
     StreamCipher as u32 with error UnsupportedStreamCipher
     {
+        /// Salsa20 algorithm with constant nonce (not expected to be used in
+        /// KeePass 4 databases)
         Salsa20 = 2,
         #[default]
+        /// ChaCha20 algorithm with both key and nonce derived from the
+        /// supplied "key"
         ChaCha20 = 3,
     }
 }
 
 impl StreamCipher {
+    /// The expected "key" size for the selected algorithm.
     pub fn key_size(&self) -> usize {
         match self {
             Self::Salsa20 => 32,
@@ -135,6 +152,10 @@ impl StreamCipher {
         }
     }
 
+    /// Creates a `cipher::StreamCipher` instance for the selected algorithm
+    /// and given "key". For Salsa20, a constant nonce will be used. For
+    /// ChaCha20, the supplied "key" is hashed and part of the result used as
+    /// key, another part as nonce.
     pub fn create(&self, key: &[u8]) -> Box<dyn cipher::StreamCipher> {
         match self {
             Self::Salsa20 => {
@@ -159,5 +180,155 @@ impl StreamCipher {
                 ))
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use hex_literal::hex;
+    use std::io::Cursor;
+
+    fn serialize<S: Serialize>(data: S) -> Vec<u8> {
+        let mut result = Vec::new();
+        data.serialize(&mut result).unwrap();
+        result
+    }
+
+    #[test]
+    pub fn test_block_deserialize() {
+        fn deserialize<const SIZE: usize>(data: [u8; SIZE]) -> Result<BlockCipher, Error> {
+            let mut cursor = Cursor::new(data);
+            BlockCipher::deserialize(&mut cursor)
+        }
+
+        assert!(matches!(
+            deserialize(hex!("")).expect_err("Deserializing empty data"),
+            Error::Io(_)
+        ));
+
+        assert!(matches!(
+            deserialize(hex!("61ab05a1 9464 41c3 8d74 3a563df8dd35"))
+                .expect_err("Deserializing AES-128 cipher"),
+            Error::UnsupportedBlockCipher
+        ));
+
+        assert_eq!(
+            deserialize(hex!("31c1f2e6 bf71 4350 be58 05216afc5aff"))
+                .expect("Deserializing AES-256 cipher"),
+            BlockCipher::Aes256
+        );
+    }
+
+    #[test]
+    pub fn test_block_serialize() {
+        assert_eq!(
+            serialize(BlockCipher::Aes256),
+            hex!("31c1f2e6 bf71 4350 be58 05216afc5aff")
+        );
+    }
+
+    #[test]
+    pub fn test_block_size() {
+        assert_eq!(BlockCipher::SIZE, 16);
+
+        assert_eq!(BlockCipher::Aes256.iv_size(), 16);
+        assert_eq!(BlockCipher::Twofish.iv_size(), 16);
+        assert_eq!(BlockCipher::ChaCha20.iv_size(), 12);
+    }
+
+    #[test]
+    pub fn test_block_encrypt_decrypt() {
+        let mut data = b"test".to_vec();
+        BlockCipher::Aes256
+            .encrypt(
+                &mut data,
+                b"01234567012345670123456701234567",
+                b"abcdefghabcdefgh",
+            )
+            .unwrap();
+        assert_eq!(data, hex!("6301DF0B911C8E1E665C4AF9F3AE8271"));
+
+        BlockCipher::Aes256
+            .decrypt(
+                &mut data,
+                b"01234567012345670123456701234567",
+                b"abcdefghabcdefgh",
+            )
+            .unwrap();
+        assert_eq!(data, b"test");
+
+        BlockCipher::ChaCha20
+            .encrypt(
+                &mut data,
+                b"01234567012345670123456701234567",
+                b"abcdefghabcd",
+            )
+            .unwrap();
+        assert_eq!(data, hex!("A1CFF2F2"));
+
+        BlockCipher::ChaCha20
+            .decrypt(
+                &mut data,
+                b"01234567012345670123456701234567",
+                b"abcdefghabcd",
+            )
+            .unwrap();
+        assert_eq!(data, b"test");
+    }
+
+    #[test]
+    pub fn test_stream_deserialize() {
+        fn deserialize<const SIZE: usize>(data: [u8; SIZE]) -> Result<StreamCipher, Error> {
+            let mut cursor = Cursor::new(data);
+            StreamCipher::deserialize(&mut cursor)
+        }
+
+        assert!(matches!(
+            deserialize(hex!("")).expect_err("Deserializing empty data"),
+            Error::Io(_)
+        ));
+
+        assert!(matches!(
+            deserialize(hex!("01 00 00 00")).expect_err("Deserializing ArcFourVariant cipher"),
+            Error::UnsupportedStreamCipher(1)
+        ));
+
+        assert_eq!(
+            deserialize(hex!("03 00 00 00")).expect("Deserializing ChaCha20 cipher"),
+            StreamCipher::ChaCha20
+        );
+    }
+
+    #[test]
+    pub fn test_stream_serialize() {
+        assert_eq!(serialize(StreamCipher::ChaCha20), hex!("03 00 00 00"));
+    }
+
+    #[test]
+    pub fn test_stream_size() {
+        assert_eq!(StreamCipher::Salsa20.key_size(), 32);
+        assert_eq!(StreamCipher::ChaCha20.key_size(), 64);
+    }
+
+    #[test]
+    pub fn test_stream_encrypt_decrypt() {
+        let mut data = b"test".to_vec();
+
+        let mut cipher = StreamCipher::Salsa20.create(b"z1234567012345670123456701234567");
+        cipher.apply_keystream(&mut data);
+        assert_eq!(data, hex!("F30E8D2E"));
+
+        let mut cipher = StreamCipher::Salsa20.create(b"z1234567012345670123456701234567");
+        cipher.apply_keystream(&mut data);
+        assert_eq!(data, b"test");
+
+        let mut cipher = StreamCipher::ChaCha20.create(b"key");
+        cipher.apply_keystream(&mut data);
+        assert_eq!(data, hex!("244CFC4D"));
+
+        let mut cipher = StreamCipher::ChaCha20.create(b"key");
+        cipher.apply_keystream(&mut data);
+        assert_eq!(data, b"test");
     }
 }
